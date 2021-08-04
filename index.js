243f8625e57fbc5bdacc13ca95c2b83992c8f36a
Elizabeth Clawson <emclawson1@gmail.com>
2021-08-03 19:50:31 -0500

    commit tree cd7cfb755569f9baa11220f87e07906409d683a9

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function addingEventListener() {
   alert('I was clicked!');
 });
 } 
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert); 
